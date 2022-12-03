4bd9418f4757ce7e95ec52694fb3dbdb4bc28711
Andy <roshitu@yandex.ru>
2022-12-04 01:25:00 +0300

    Рефакторинг

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-const shop = document.querySelector('.shop');
-const circle = shop.querySelectorAll('.surfboard-box__circle');
-
-shop.addEventListener('click', (evt) => {
-  if (evt.target.closest('.surfboard-box__circle')) {
-    evt.target.closest('.surfboard-box__circle').classList.toggle('circle--minus')
-  }
-})
